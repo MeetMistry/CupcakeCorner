c7d20dfbe8d798f30715849b6a3d5418a6498640
Meet Mistry <meetmistry138@gmail.com>
2024-10-30 08:17:12 +0530

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Meet Mistry on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
